47fcd3f88c32457a513550bec7854bc6fca16b80
Iva Bozic <ivabozic23@gmail.com>
2020-08-05 12:32:33 +0100

    added state to app / form to create

--- client/src/App.js
+++ client/src/App.js
@@ -8,17 +8,45 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      //currently "logged in" user
       user: 1,
+      rides: [],
     };
   }
 
+  addNewRide = async (newRide) => {
+    //send ride to db
+    //get new list of rides
+    //update state of rides
+  };
+
+  componentDidMount = async () => {
+    try {
+      const res = await fetch("/api/rides");
+      const rides = await res.json();
+      this.setState({
+        rides: rides,
+      });
+    } catch (err) {
+      // upon failure, show error message
+      console.log(err);
+    }
+  };
+
   render() {
-    const { user } = this.state;
+    const { user, rides } = this.state;
     return (
       <div className="App">
-        <MyRides user={user} />
-        <CreateRide user={user} />
-        <AllRides user={user} />
+        Main app
+        <div>
+          {/* <MyRides user={user} rides={rides} /> */}
+          <CreateRide
+            user={user}
+            rides={rides}
+            addNewRide={(newRide) => this.addNewRide(newRide)}
+          />
+          {/* <AllRides user={user} rides={rides} /> */}
+        </div>
       </div>
     );
   }
--- client/src/components/CreateRide.js
+++ client/src/components/CreateRide.js
@@ -1,7 +1,94 @@
 import React, { Component } from "react";
 
 export default class createRide extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startdate: "",
+      startpoint: "",
+      title: "",
+      description: "",
+      difficulty: "",
+      terraintype: "",
+      lengthinkm: 0,
+      createdby: this.props.user,
+    };
+  }
+
+  handleInputChange(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>CREATE RIDE</div>;
+    const {
+      startdate,
+      startpoint,
+      title,
+      description,
+      difficulty,
+      terraintype,
+      lengthinkm,
+    } = this.state;
+    return (
+      <div>
+        CREATE RIDE
+        <form>
+          Title
+          <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={(e) => this.handleInputChange(e)}
+          ></input>
+          Starting date and time
+          <input
+            type="datetime-local"
+            name="startdate"
+            value={startdate}
+            onChange={(e) => this.handleInputChange(e)}
+          ></input>
+          Start address{" "}
+          <input
+            type="text"
+            name="startpoint"
+            value={startpoint}
+            onChange={(e) => this.handleInputChange(e)}
+          ></input>
+          Description
+          <textarea
+            name="description"
+            value={description}
+            onChange={(e) => this.handleInputChange(e)}
+          ></textarea>
+          Terrain type
+          <input
+            type="text"
+            name="terraintype"
+            value={terraintype}
+            onChange={(e) => this.handleInputChange(e)}
+          ></input>
+          Length
+          <input
+            type="number"
+            name="lengthinkm"
+            value={lengthinkm}
+            onChange={(e) => this.handleInputChange(e)}
+          ></input>
+          Difficulty
+          <input
+            type="text"
+            name="difficulty"
+            value={difficulty}
+            onChange={(e) => this.handleInputChange(e)}
+          ></input>
+          <button>Add</button>
+        </form>
+      </div>
+    );
   }
 }
