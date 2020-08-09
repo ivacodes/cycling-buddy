3c58a517849d57196c92c33271824f83746a0cbe
Iva Bozic <ivabozic23@gmail.com>
2020-08-09 14:05:04 +0100

    myrides css half done

--- client/src/components/MyRides.js
+++ client/src/components/MyRides.js
@@ -31,62 +31,68 @@ export default class MyRides extends Component {
     const { myRideDetails } = this.state;
     return (
       <div>
-        LIST OF MY RIDES
-        <div>
-          {usersRides.map((ride) => {
-            return (
-              <div
-                key={ride.ride_id}
-                onClick={() => {
-                  this.onSelectRide(ride);
-                }}
-              >
-                <strong>{ride.title}</strong>
-                <br />
-                <em>{ride.description}</em>
-              </div>
-            );
-          })}
+        <div className="row">
+          <br />
         </div>
-        details{" "}
-        <div>
-          {myRideDetails != null ? (
-            <div>
-              <div>{myRideDetails.title}</div>
+        <div className="row">
+          <div className="col-6">
+            {usersRides.map((ride) => {
+              return (
+                <div
+                  key={ride.ride_id}
+                  onClick={() => {
+                    this.onSelectRide(ride);
+                  }}
+                  className="text-info mb-3"
+                >
+                  <strong className="h5">{ride.title}</strong>
+                  <br />
+                  <span className="text-muted">{ride.startdate}</span>
+                  <br />
+                  <span className="text-muted">{ride.startpoint}</span>
+                </div>
+              );
+            })}
+          </div>
+          <div className="col-6">
+            {myRideDetails != null ? (
               <div>
-                <p>{myRideDetails.description}</p>
+                <div>{myRideDetails.title}</div>
+                <div>
+                  <p>{myRideDetails.description}</p>
+                </div>
+                <div>
+                  <span>Where: </span>
+                  {myRideDetails.startpoint}
+                </div>
+                <div>
+                  <span>When: </span>
+                  {myRideDetails.startdate}
+                </div>
+                <div>
+                  <span>Length: </span>
+                  {myRideDetails.lengthinkm} km
+                </div>
+                <div>
+                  <span>Difficulty: </span>
+                  {myRideDetails.difficulty}
+                </div>
+                <div>
+                  <span>Terrain type: </span>
+                  {myRideDetails.terraintype}
+                </div>
+                <button
+                  onClick={(e) => {
+                    this.deleteButtonPressed(e);
+                  }}
+                >
+                  Remove
+                </button>
               </div>
-              <div>
-                <span>Where: </span>
-                {myRideDetails.startpoint}
-              </div>
-              <div>
-                <span>When: </span>
-                {myRideDetails.startdate}
-              </div>
-              <div>
-                <span>Length: </span>
-                {myRideDetails.lengthinkm} km
-              </div>
-              <div>
-                <span>Difficulty: </span>
-                {myRideDetails.difficulty}
-              </div>
-              <div>
-                <span>Terrain type: </span>
-                {myRideDetails.terraintype}
-              </div>
-              <button
-                onClick={(e) => {
-                  this.deleteButtonPressed(e);
-                }}
-              >
-                Remove
-              </button>
-            </div>
-          ) : (
-            <div></div>
-          )}
+            ) : (
+              <div></div>
+            )}
+          </div>
         </div>
       </div>
     );
